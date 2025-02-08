290ac97cc0435ac37d5a7628903e06b79e5c5b34
adityatulsyan03 <tulsyanaditya03@gmail.com>
2025-02-08 17:30:45 +0530

    Feat - OnBoarding Screen & Create Team Screen

---
+++ app/src/main/java/in/iotkiit/raidersreckoningapp/view/screens/OnBoardingScreen.kt
@@ -0,0 +1,110 @@
+package `in`.iotkiit.raidersreckoningapp.view.screens
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.wear.compose.material.Button
+import androidx.wear.compose.material.ButtonDefaults
+import `in`.iotkiit.raidersreckoningapp.R
+import `in`.iotkiit.raidersreckoningapp.view.navigation.RaidersReckoningScreens
+import `in`.iotkiit.raidersreckoningapp.vm.DashBoardViewModel
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun OnBoardingScreen(
+    navController: NavController,
+    viewModel: DashBoardViewModel = hiltViewModel()
+) {
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(R.drawable.background),
+                contentDescription = "Background",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.title),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillWidth,
+                    modifier = Modifier.fillMaxWidth(0.75f)
+                )
+
+                Column {
+                    Button(
+                        onClick = {
+                            navController.navigate(RaidersReckoningScreens.CreateTeamScreen.route)
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth(0.7f)
+                            .border(2.dp, Color.Green, RoundedCornerShape(50)),
+                        colors = ButtonDefaults.primaryButtonColors(
+                            backgroundColor = Color.Transparent
+                        )
+                    ) {
+                        Text(
+                            text = "CREATE TEAM",
+                            fontSize = 24.sp,
+                            color = Color.White,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                    Spacer(Modifier.height(16.dp))
+                    Button(
+                        onClick = {
+                            //Join Team Screen
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth(0.7f)
+                            .border(2.dp, Color.Green, RoundedCornerShape(50)),
+                        colors = ButtonDefaults.primaryButtonColors(
+                            backgroundColor = Color.Transparent
+                        )
+                    ) {
+                        Text(
+                            "JOIN TEAM",
+                            fontSize = 24.sp,
+                            color = Color.White,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+
+            }
+        }
+    }
+
+}
