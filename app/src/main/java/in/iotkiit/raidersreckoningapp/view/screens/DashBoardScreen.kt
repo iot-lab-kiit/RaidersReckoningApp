daf233eb9961b76e04e9960269b3e0fc843c1c26
Kunal Saha <kunalsaha1107@gmail.com>
2025-02-12 18:42:18 +0530

    fix - api calling in DashBoardScreen and MyTeamScreen

--- app/src/main/java/in/iotkiit/raidersreckoningapp/view/screens/DashBoardScreen.kt
+++ app/src/main/java/in/iotkiit/raidersreckoningapp/view/screens/DashBoardScreen.kt
@@ -5,116 +5,214 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import `in`.iotkiit.raidersreckoningapp.R
+import `in`.iotkiit.raidersreckoningapp.state.UiState
 import `in`.iotkiit.raidersreckoningapp.ui.theme.GreenCOD
 import `in`.iotkiit.raidersreckoningapp.view.components.core.topbar.TopBar
 import `in`.iotkiit.raidersreckoningapp.view.components.dashboard.MapCard
 import `in`.iotkiit.raidersreckoningapp.view.navigation.BottomNavBar
 import `in`.iotkiit.raidersreckoningapp.view.navigation.BottomNavOptions.Companion.bottomNavOptions
 import `in`.iotkiit.raidersreckoningapp.vm.DashBoardViewModel
+import kotlinx.coroutines.delay
 
 @Composable
 fun DashBoardScreen(
-    navController: NavController, viewModel: DashBoardViewModel = hiltViewModel()
+    navController: NavController,
+    dashBoardViewModel: DashBoardViewModel = hiltViewModel()
 ) {
+    val dashBoardState = dashBoardViewModel.getDashBoardState.collectAsState().value
+    var remainingTime by remember { mutableStateOf(0L) }
 
-    val getDashBoardState = viewModel.getDashBoardState.collectAsState().value
+    when(dashBoardState) {
+        is UiState.Idle -> {
+            dashBoardViewModel.getDashBoardData()
+        }
 
-    Scaffold(containerColor = Color.Black, bottomBar = {
-        BottomNavBar(navController = navController, bottomMenu = bottomNavOptions)
-    }, topBar = {
-        TopBar(
-            modifier = Modifier.fillMaxWidth(), teamName = "TaskForce141", points = 10
-        )
-    }) {
-
-        Column(
-            modifier = Modifier
-                .padding(it)
-                .padding(16.dp)
-                .fillMaxSize()
-                .background(Color.Black),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-
-            MapCard(
-                mapImage = painterResource(R.drawable.map),
-                soldierImage = painterResource(R.drawable.soldier)
-            )
-
+        is UiState.Loading -> {
             Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(16.dp)
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(
-                        space = 24.dp, alignment = Alignment.CenterHorizontally
-                    ),
-                    verticalAlignment = Alignment.Bottom
-                ) {
-                    Text(
-                        text = "NUKETOWN",
-                        style = MaterialTheme.typography.displayMedium,
-                        color = Color.White
-                    )
-                    Text(
-                        modifier = Modifier,
-                        text = "C001",
-                        style = MaterialTheme.typography.labelSmall,
-                        color = GreenCOD
-                    )
-                }
+                LinearProgressIndicator(color = GreenCOD)
+            }
+        }
 
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceAround,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = "Round 1",
-                        style = MaterialTheme.typography.bodyMedium,
-                        color = GreenCOD
-                    )
+        is UiState.Success -> {
 
-                    Text(
-                        color = Color.White,
-                        text = "30:00",
-                        style = MaterialTheme.typography.bodyMedium
-                    )
+            val data = dashBoardState.data.data
+            val teamName = data?.teamName?:"NoTeam"
+            val points = data?.points ?: 0
+            val zoneName = data?.zone?.name?:"No zone"
+            val zoneVenue = data?.zone?.venue?:"assigned"
+            val round = data?.round ?: 0
+            val startTime = data?.zone?.startTime ?: 0L
+            val duration = data?.zone?.duration ?: 0L
+            val challenger = data?.isChallenger ?: true
+            //logic for timer
+            LaunchedEffect(data?.zone?.startTime) {
+                while (remainingTime > 0) {
+                    val currentTime = System.currentTimeMillis()
+                    val endTime = startTime + duration
+                    remainingTime = (endTime - currentTime).coerceAtLeast(0)
+                    delay(1000) // Update every second
                 }
             }
 
-            FloatingActionButton(
-                onClick = { /*TODO*/ },
-                containerColor = GreenCOD,
-                contentColor = Color.Black,
+            Scaffold(
+                containerColor = Color.Black,
+                bottomBar = {
+                    BottomNavBar(navController = navController, bottomMenu = bottomNavOptions)
+                },
+                topBar = {
+                    TopBar(
+                        teamName = teamName,
+                        points = points
+                    )
+                }
             ) {
-                Image(
-                    modifier = Modifier.padding(8.dp),
-                    painter = painterResource(R.drawable.scan),
-                    contentDescription = null
+                Column(
+                    modifier = Modifier
+                        .padding(it)
+                        .padding(16.dp)
+                        .fillMaxSize()
+                        .background(Color.Black),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "Round $round",
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = GreenCOD
+                        )
+
+                        val minutes = (remainingTime / 1000 / 60).toInt()
+                        val seconds = ((remainingTime / 1000) % 60).toInt()
+
+                        Text(
+                            text = String.format("%02d:%02d", minutes, seconds),
+                            color = when {
+                                remainingTime <= 300000 -> Color.Red  // Less than 5 minutes
+                                remainingTime <= 600000 -> Color.Yellow // Less than 10 minutes
+                                else -> Color.White
+                            },
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                    MapCard(
+                        mapImage = painterResource(R.drawable.map),
+                        soldierImage = painterResource(R.drawable.soldier)
+                    )
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.Bottom
+                    ) {
+                        Text(
+                            text = zoneName,
+                            style = MaterialTheme.typography.displayMedium,
+                            color = Color.White
+                        )
+                        Text(
+                            modifier = Modifier.padding(start = 5.dp, bottom = 5.dp),
+                            text = zoneVenue,
+                            style = MaterialTheme.typography.labelSmall,
+                            color = GreenCOD
+                        )
+                    }
+                    Column(
+                        modifier = Modifier.fillMaxSize(0.75f),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        if (challenger){
+                            Text(
+                                text = "You are a challenger!",
+                                textAlign = TextAlign.Center,
+                                color = Color.Yellow,
+                                style = MaterialTheme.typography.displaySmall
+                            )
+                        } else {
+                            Text(
+                                text = "You are not a challenger",
+                                textAlign = TextAlign.Center,
+                                color = Color.Red,
+                                style = MaterialTheme.typography.displaySmall
+                            )
+                        }
+
+                    }
+
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Bottom,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        FloatingActionButton(
+                            onClick = { /*TODO*/ },
+                            containerColor = GreenCOD,
+                            contentColor = Color.Black,
+                        ) {
+                            Image(
+                                modifier = Modifier.padding(8.dp),
+                                painter = painterResource(R.drawable.scan),
+                                contentDescription = null
+                            )
+                        }
+                    }
+                }
+            }
+        }
+
+        is UiState.Failed -> {
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.background),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = dashBoardState.message,
+                    color = MaterialTheme.colorScheme.error,
+                    style = MaterialTheme.typography.bodyLarge
                 )
             }
         }
     }
-
 }
--- app/src/main/java/in/iotkiit/raidersreckoningapp/view/screens/MyTeamScreen.kt
+++ app/src/main/java/in/iotkiit/raidersreckoningapp/view/screens/MyTeamScreen.kt
@@ -1,12 +1,18 @@
 package `in`.iotkiit.raidersreckoningapp.view.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -15,92 +21,104 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import `in`.iotkiit.raidersreckoningapp.state.UiState
 import `in`.iotkiit.raidersreckoningapp.ui.theme.GreenCOD
 import `in`.iotkiit.raidersreckoningapp.view.components.core.topbar.TopBar
 import `in`.iotkiit.raidersreckoningapp.view.components.myTeam.Fields
 import `in`.iotkiit.raidersreckoningapp.view.navigation.BottomNavBar
 import `in`.iotkiit.raidersreckoningapp.view.navigation.BottomNavOptions.Companion.bottomNavOptions
 import `in`.iotkiit.raidersreckoningapp.vm.DashBoardViewModel
+import `in`.iotkiit.raidersreckoningapp.vm.LeaderboardViewModel
+import `in`.iotkiit.raidersreckoningapp.vm.TeamViewModel
 
 @Composable
 fun MyTeamScreen(
     navController: NavController,
-    dashBoardViewModel: DashBoardViewModel = hiltViewModel()
+    teamViewModel: TeamViewModel = hiltViewModel(),
+    leaderboardViewModel: LeaderboardViewModel = hiltViewModel()
 ) {
 
-    val dashBoardState = dashBoardViewModel.getDashBoardState.collectAsState().value
+    val teamState = teamViewModel.getTeamState.collectAsState().value
+    val leaderboardState = leaderboardViewModel.getLeaderboardData.collectAsState().value
 
-    Scaffold(
-        containerColor = Color.Black,
-        modifier = Modifier.fillMaxSize(),
-        topBar = {
-            TopBar(
-                modifier = Modifier.fillMaxWidth(),
-                teamName = "TaskForce141",
-                points = 10
-            )
-        },
-        bottomBar = {
-            BottomNavBar(navController = navController, bottomMenu = bottomNavOptions)
+    when (teamState) {
+        is UiState.Idle -> {
+            teamViewModel.getTeam()
         }
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize().padding(it).background(Color.Black),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+
+        is UiState.Loading -> {
             Column(
-                modifier = Modifier,
-                verticalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black),
+                verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Fields(
-                    field = "Taskforce141",
-                    containerColor = GreenCOD,
-                    contentColor = Color.Black,
-                )
-                Fields("Kunal")
-                Fields("Binayak")
-                Fields("Sarthak")
+                LinearProgressIndicator(color = GreenCOD)
             }
         }
+
+        is UiState.Success -> {
+            val data = teamState.data.data
+            val teamName = data?.name ?:"NoTeam"
+            val points = data?.points ?: 0
+            val leaderName = data?.leaderInfo?.name ?: ""
+            val participantsList = data?.participantsList ?: emptyList()
+
+            Scaffold(
+                containerColor = Color.Black,
+                modifier = Modifier.fillMaxSize(),
+                topBar = {
+                    TopBar(
+                        teamName = teamName,
+                        points = points
+                    )
+                },
+                bottomBar = {
+                    BottomNavBar(navController = navController, bottomMenu = bottomNavOptions)
+                }
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize()
+                        .padding(it)
+                        .background(Color.Black),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(4.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Fields(
+                            field = leaderName,
+                            containerColor = GreenCOD,
+                            contentColor = Color.Black,
+                        )
+                        LazyColumn {
+                            items(participantsList) { participant ->
+                                Fields(participant.name)
+                            }
+                        }
+                    }
+                }
+            }
         }
 
-//        when (getDashBoardState) {
-//            is UiState.Idle -> {
-//                dashBoardViewModel.getDashBoardData("")
-//            }
-//
-//            is UiState.Loading -> {
-//                Column(
-//                    modifier = Modifier
-//                        .fillMaxSize(),
-//                    verticalArrangement = Arrangement.Center,
-//                    horizontalAlignment = Alignment.CenterHorizontally
-//                ) {
-//                    LinearProgressIndicator(color = GreenCOD)
-//                }
-//            }
-//
-//            is UiState.Success -> {
-//                Surface(
-//                    modifier = Modifier
-//                        .fillMaxSize()
-//                        .safeContentPadding()
-//                ) {
-//                    Fields("Kunal")
-//                }
-//            }
-//
-//            is UiState.Failed -> {
-//                Column(
-//                    modifier = Modifier
-//                        .fillMaxSize(),
-//                    verticalArrangement = Arrangement.Center,
-//                    horizontalAlignment = Alignment.CenterHorizontally
-//                ) {
-//                    Text(text = "FAILED!")
-//                }
-//            }
-//        }
+        is UiState.Failed -> {
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.background),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = teamState.message,
+                    color = MaterialTheme.colorScheme.error,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+        }
+    }
 }
